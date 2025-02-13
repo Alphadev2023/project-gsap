e3f04c6f3bf40c562210ca0e25aa6685b74734b0
alphadev2023 <alphadev2023@gmail.com>
2025-02-13 10:22:23 +0100

    I do a other commit2

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,11 @@
 let getProduct = document.getElementById("products");
 
-async function getData(file){
- let getFetched = await fetch(file);
- let data =  await getFetched.text()
- console.log(data);
+async function getData(){
+  fetch('./data.json')
+   .then( res => res.json() )
+   .then( (data) => {
+      console.log(data)
+   })
 }
 
-getData("../js/data.json")
-
-getProduct.innerHTML = `
-
-
-
-`;
+getData();
