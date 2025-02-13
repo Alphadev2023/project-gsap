48a0d540db8601f37ba3f37a6dc4b1b591488c15
alphadev2023 <alphadev2023@gmail.com>
2025-02-13 13:56:30 +0100

    I do a other commit2

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,28 @@
 let getProduct = document.getElementById("products");
 
-async function getData(){
+document.addEventListener("DOMContentLoaded", ()=>{
+ async function getData(){
   fetch("http://127.0.0.1:5501/assets/js/data.json")
    .then( res => res.json() )
-   .then( (data) => {
-      console.log(data);
-   });
+   .then( (data) => addData(data));
 }
 
+function addData(data){
+ for(let dat in data){
+  const content = document.createElement("div");
+  let image = document.createElement("img");
+  let title = document.createElement("h2");
+  let para = document.createElement("p");
+
+  content.className = "content-element";
+  image.src = dat.image;
+  title.innerHTML = dat.name;
+  para.innerHTML = dat.price;
+
+  content.append(image, title, price);
+  getProduct.append(content);
+ }
+}
+getData();
+})
 
